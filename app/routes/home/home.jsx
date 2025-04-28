0bb543f5544cc911ac3f96d7459ca0b3e15cce90
Aditya <aaak1837@gmail.com>
2025-04-28 21:45:02 +0530

    updated

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -43,7 +43,7 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'UI/UX Designer Portfolio',
+    title: 'UI/UX Designer Portfolio ',
     description: `Design portfolio of Astha Mishra — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
   });
 };
